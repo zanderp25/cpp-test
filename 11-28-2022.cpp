8adafe0fb19d8a3f130dfe4ded7d3972119d0099
zanderp25 <zanderpicon@outlook.com>
2022-11-30 20:41:09 +0000

    11-28-2022 Part 2: Electric Bungaloo

--- 11-28-2022.cpp
+++ 11-28-2022.cpp
@@ -11,8 +11,10 @@ int main(){
     const int ARR_SIZE = 16;
     // Array Declaration
     int read_array[ARR_SIZE];
+
     // Array Initialization
-    int write_array[ARR_SIZE-8] = {1,2,3,4,5,6,7,8};
+    // int write_array[ARR_SIZE-8] = {1,2,3,4,5,6,7,8};
+    int write_array[] = {1,2,3,4,5,6,7,8};
 
     // file streams
     ifstream file_in;
@@ -25,12 +27,14 @@ int main(){
     file_out.close();
 
     file_in.open("ファイル.txt");
-    for(int i=0; i < ARR_SIZE; i++){
+    // for(int i=0; i < ARR_SIZE; i++){
+    for(int i=0; i < ARR_SIZE-8; i++){
         file_in >> read_array[i];
     }
     file_in.close();
 
-    for(int i=0; i < ARR_SIZE; i++){
+    // for(int i=0; i < ARR_SIZE; i++){
+    for(int i=0; i < ARR_SIZE-8; i++){
         cout << i << ": " << read_array[i] << endl;
     }
 }
