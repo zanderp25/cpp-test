0bf530db29dfdfe4f6bbbd443c744ea5d2e33983
zanderp25 <zanderpicon@outlook.com>
2022-12-05 20:44:03 +0000

    12-02-2022

---
+++ 12-02-2022.cpp
@@ -0,0 +1,56 @@
+/*  Name: Functions
+    Date: 11-30-2022
+*/
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+// function definition
+// <return datatype> function_name(arguments, parameters) {}
+int meow(string str){
+    // function body
+    cout << "ニャア〜" << endl;
+
+    cout << "猫は「" << str << "」と言いました。" << endl;
+
+    return 23;
+}
+
+double woof(){
+    cout << "¡Guau!" << endl;
+
+    return 23e-5;
+}
+
+bool moo(){
+    cout << "Moooooooooooo!" << endl;
+
+    return true;
+}
+
+int main(){
+    int meow_var;
+    double woof_var;
+    bool moo_var;
+
+    int sum_var;
+
+    // string quote;
+    // cout << "What did the cat say? > ";
+    // readLine
+
+    // function call
+    meow_var = meow(quote);
+    woof_var = woof();
+    moo_var = moo();
+
+    cout << "The cat has meowed " << meow_var << " times." << endl;
+    cout << "The dog will bark " << woof_var << " times." << endl;
+    cout << "The cow has mooed: " << (moo_var ? "true": "false") << endl;
+
+    sum_var = meow_var + woof_var + moo_var;
+
+    cout << "\nSum of returned values:" << sum_var << endl;
+}
